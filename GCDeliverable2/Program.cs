d17b7da0e9f7948fced3e74878dcf9c13b9d518f
christina <clambert0615@gmail.com>
2020-02-14 12:30:31 -0500

    This is the second deliverable

---
+++ GCDeliverable2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace GCDeliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            byte[] message;
+            int checksum = 0;
+            ASCIIEncoding ascii = new ASCIIEncoding();
+
+            Console.WriteLine("Please enter a word.");
+            input = Console.ReadLine();
+            input = input.ToUpper();
+
+            message = ascii.GetBytes(input);
+            Console.WriteLine("Your encoded message is:");
+
+            foreach (Byte b in message)
+            {
+                Console.Write("{0}-", b - 65);
+                checksum = checksum + b;
+            }
+            Console.WriteLine();
+           
+            Console.WriteLine("Message checksum is " + checksum);
+        }
+    }
+}
